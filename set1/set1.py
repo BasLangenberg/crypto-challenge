b42695681d849b3665c84b1f7234c96f4a4c6f14
Bas Langenberg <baslangenberg@gmail.com>
2017-02-28 16:25:23 +0100

    Add solution for second problem

--- set1/set1.py
+++ set1/set1.py
@@ -2,6 +2,14 @@ import codecs
 import base64
 
 
+def hex_to_bytes(input):
+    return codecs.decode(input, 'hex_codec')
+
+
 def hex_to_base64(hex_string):
     hex_decode = codecs.decode(hex_string, 'hex_codec')
     return base64.b64encode(hex_decode)
+
+
+def xor_hex(a, b):
+    return hex(int(a, 16) ^ int(b, 16))
--- set1/tests_set1.py
+++ set1/tests_set1.py
@@ -1,6 +1,16 @@
 import set1
 
 
+# Challenge 1
 def test_hex_to_base64():
     input = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
     assert set1.hex_to_base64(input) == b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+
+# Challenge 2
+def test_xor_string():
+    s1 = '1c0111001f010100061a024b53535009181c'
+    s2 = '686974207468652062756c6c277320657965'
+    expect = '746865206b696420646f6e277420706c6179'
+
+    assert set1.xor_hex(s1, s2) == hex(int(expect, 16))
